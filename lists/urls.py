c159fd12d4280e7b2b6bc66b646c3f71c958fcb4
Nitin Savant <nitinsavant@gmail.com>
2015-12-23 17:20:36 +0700

    Add suburl for movies

--- lists/urls.py
+++ lists/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', include('movies.urls', namespace="movies")),
+    url(r'^movies/', include('movies.urls', namespace="movies")),
 ]
