5a6100aafae730fd2ec07896a0dd62847403e819
Nitin Savant <nitinsavant@gmail.com>
2015-12-23 17:57:40 +0700

    Added author field

---
+++ books/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Book(models.Model):
+	title = models.CharField(max_length=100)
+        author = models.CharField(max_length=100)
+	release_date = models.DateField(null=True, blank=True)
+	watch_date = models.DateField(default='1900-01-01')
+	title_link = models.URLField(null=True, blank=True)
+
+	def __str__(self):
+		return self.title
